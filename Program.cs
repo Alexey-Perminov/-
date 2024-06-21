137a5cfb7456872524f09e08e4532aea1f21070e
Alexey <Lexa8keis@gmail.com>
2024-06-21 16:11:04 +0900

    Написал первое решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива
+// строк формирует новый массив из строк, длина которых меньше,
+// либо равна 3 символам. Первоначальный массив можно ввести
+// с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше
+// обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+// Первое решение
+string[] str = ["Helllo", "World", "no", "2", "Hi", "Russia", "Kazan", ":-)", "1234", "567"];
+System.Console.WriteLine("Исходный массив: ");
+foreach (string s in str)
+{
+    System.Console.Write(s + " ");
+}
+System.Console.WriteLine("\n");
+System.Console.WriteLine("Новый массив: ");
+foreach (string s in str)
+{
+    if (s.Length <= 3)
+    {
+        System.Console.Write(s + " ");
+    }
+}
+
